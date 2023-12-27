94098bbba455c572b3d22e31e06fa99a6732653e
bp4634 <mpierreslantigua@gmail.com>
2023-12-27 13:28:46 -0400

    last version of cache

--- cache/app.js
+++ cache/app.js
@@ -8,23 +8,85 @@ const BtnCGet = document.getElementById('BtnCGet')
 const NameL = document.getElementById("NameL")
 const LastNameL = document.getElementById("LastnameL")
 const NameS = document.getElementById('NameS')
-const LastNameS = document.getElementById('LastNameS')
+const LastNameS = document.getElementById('LastnameS')
 const NameC = document.getElementById('NameC')
-const LastNameC = document.getElementById('LastNameC')
+const LastNameC = document.getElementById('LastnameC')
 const NameLGet = document.getElementById('NameLGet')
-const LastNameLGet = document.getElementById('LastNameLGet')
+const LastNameLGet = document.getElementById('LastnameLGet')
 const NameSGet = document.getElementById('NameSGet')
-const LastNameSGet = document.getElementById('LastNameSGet')
+const LastNameSGet = document.getElementById('LastnameSGet')
 const NameCGet = document.getElementById('NameCGet')
-const LastNameCGet = document.getElementById('LastNameCGet')
+const LastNameCGet = document.getElementById('LastnameCGet')
+
+//LOCAL STORAGE
 
 createBtnL.addEventListener("click", () => {
-    createLocalStorage(NameL.value, LastNameL.value)
+
+    localStorage.setItem('Name', NameL.value)
+    localStorage.setItem('Last name',LastNameL.value)
 })
 
 
-function createLocalStorage(name, lastName) {
+BtnLGet.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    NameLGet.value = localStorage.getItem('Name')
     
-    localStorage.setItem('Name',name)
-    localStorage.setItem('Last name',lastName)
-}
+    LastNameLGet.value = localStorage.getItem('Last name')
+})
+
+
+//SESSION STORAGE
+createBtnS.addEventListener("click", () => {
+
+    sessionStorage.setItem('Name', NameS.value)
+    sessionStorage.setItem('Last name',LastNameS.value)
+})
+
+
+BtnSGet.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    NameSGet.value = sessionStorage.getItem('Name')
+    
+    LastNameSGet.value = sessionStorage.getItem('Last name')
+})
+
+
+//COOKIES STORAGE
+
+createBtnC.addEventListener("click", (e) => {
+    e.preventDefault()
+    document.cookie = `Name=${NameC.value}`
+    document.cookie = `Last name=${LastNameC.value}`
+    console.log(document.cookie)
+})
+
+
+BtnCGet.addEventListener("click", (e) => {
+    e.preventDefault()
+
+     
+    cArray = decodeURIComponent(document.cookie).split('; ')
+    
+    
+    cArray.forEach((element)=> {
+        if(element.indexOf('Name') == 0) {
+            NameCGet.value = element.substring(5)
+        }
+
+        if(element.indexOf('Last') == 0) {
+            LastNameCGet.value = element.substring(10)
+        }
+
+        
+        
+        
+    })
+    
+    
+})
+
+
+
+
