5560de5dd1b42f17776d29d3dcfdfe795ce66d3d
bp4634 <mpierreslantigua@gmail.com>
2024-01-02 22:54:36 -0400

    raw code complete

--- Gallery/app.js
+++ Gallery/app.js
@@ -19,8 +19,10 @@ const picsCity = city.querySelectorAll('.img')
 modalImages = modal.querySelectorAll('.modal-img')
 modalImg = modal.querySelector('.modal-images')
 const closeBtn = modal.querySelector('.close-btn')
+const prevBtn = modal.querySelector('.prev-btn')
+const nextBtn = modal.querySelector('.next-btn')
 
-picsNature.forEach((element) => {
+picsNature.forEach((element,index) => {
   element.addEventListener('click', () => {
     mainImg.src = element.src
     
@@ -37,11 +39,75 @@ picsNature.forEach((element) => {
     })
 
     modal.classList.add('open')
+
+
+    nextBtn.addEventListener('click', () => {
+  
+      index === picsNature.length-1 ? index = 0 : index++
+      
+        mainImg.src = picsNature[index].src
+        imgName.innerHTML = picsNature[index].title
+    
+        modalImg.innerHTML = ''
+        picsNature.forEach(e => {
+          e.classList.add('modal-img')
+          modalImg.appendChild(e)
+          if(e === picsNature[index]) {
+            picsNature.forEach(element => element.classList.remove('selected'))
+            e.classList.add('selected')
+          }
+        })
+  
+  
+  
+  
+      
+    })
+
+
+
+    prevBtn.addEventListener('click', () => {
+  
+      index === 0 ? index = picsNature.length-1 : index--
+      
+        mainImg.src = picsNature[index].src
+        imgName.innerHTML = picsNature[index].title
+    
+        modalImg.innerHTML = ''
+        picsNature.forEach(e => {
+          e.classList.add('modal-img')
+          modalImg.appendChild(e)
+          if(e === picsNature[index]) {
+            picsNature.forEach(element => element.classList.remove('selected'))
+            e.classList.add('selected')
+          }
+        })
+  
+  
+  
+  
+      
+    })
+
+
+
   })
+
+
+  closeBtn.addEventListener('click',() => {
+    modal.classList.remove('open')
+    picsNature.forEach(e => {
+      nature.appendChild(e)
+      e.classList.remove('modal-img')
+    })
+    
+  })
+
+
 });
 
 
-picsCity.forEach((element) => {
+picsCity.forEach((element,index) => {
   element.addEventListener('click', () => {
     mainImg.src = element.src
     
@@ -58,21 +124,68 @@ picsCity.forEach((element) => {
     })
 
     modal.classList.add('open')
-  })
-});
 
 
-
-closeBtn.addEventListener('click',() => {
-  modal.classList.remove('open')
-  picsNature.forEach(e => {
-    nature.appendChild(e)
-    e.classList.remove('modal-img')
-  })
-  picsCity.forEach(e => {
-    city.appendChild(e)
-    e.classList.remove('modal-img')
-  })
+    nextBtn.addEventListener('click', () => {
+  
+      index === picsCity.length-1 ? index = 0 : index++
+      
+        mainImg.src = picsCity[index].src
+        imgName.innerHTML = picsCity[index].title
+    
+        modalImg.innerHTML = ''
+        picsCity.forEach(e => {
+          e.classList.add('modal-img')
+          modalImg.appendChild(e)
+          if(e === picsCity[index]) {
+            picsCity.forEach(element => element.classList.remove('selected'))
+            e.classList.add('selected')
+          }
+        })
   
   
+  
+  
+      
+    })
+
+
+
+
+    prevBtn.addEventListener('click', () => {
+  
+      index === 0 ? index = picsCity.length-1 : index--
+      
+        mainImg.src = picsCity[index].src
+        imgName.innerHTML = picsCity[index].title
+    
+        modalImg.innerHTML = ''
+        picsCity.forEach(e => {
+          e.classList.add('modal-img')
+          modalImg.appendChild(e)
+          if(e === picsCity[index]) {
+            picsCity.forEach(element => element.classList.remove('selected'))
+            e.classList.add('selected')
+          }
+        })
+  
+  
+  
+  
+      
+    })
+
+
+  })
+
+  closeBtn.addEventListener('click',() => {
+    modal.classList.remove('open')
+
+    picsCity.forEach(e => {
+      city.appendChild(e)
+      e.classList.remove('modal-img')
+    })
+    
+    
+  })
 })
